17922bd055ff5c65fbfdc356fbe7ceb1a6ef297f
Ahmad Omar <31226496+AhmadEOmar@users.noreply.github.com>
2017-11-27 09:10:57 -0500

    Add files via upload

---
+++ GasPump/GasPump/GasPump.Designer.vb
@@ -0,0 +1,123 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class GasPump
+   Inherits System.Windows.Forms.Form
+
+   'Form overrides dispose to clean up the component list.
+   <System.Diagnostics.DebuggerNonUserCode()> _
+   Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+      Try
+         If disposing AndAlso components IsNot Nothing Then
+            components.Dispose()
+         End If
+      Finally
+         MyBase.Dispose(disposing)
+      End Try
+   End Sub
+
+   'Required by the Windows Form Designer
+   Private components As System.ComponentModel.IContainer
+
+   'NOTE: The following procedure is required by the Windows Form Designer
+   'It can be modified using the Windows Form Designer.  
+   'Do not modify it using the code editor.
+   <System.Diagnostics.DebuggerStepThrough()> _
+   Private Sub InitializeComponent()
+        Me.superButton = New System.Windows.Forms.Button()
+        Me.specialButton = New System.Windows.Forms.Button()
+        Me.regularButton = New System.Windows.Forms.Button()
+        Me.totalResultLabel = New System.Windows.Forms.Label()
+        Me.totalLabel = New System.Windows.Forms.Label()
+        Me.gallonsTextBox = New System.Windows.Forms.TextBox()
+        Me.gallonsLabel = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'superButton
+        '
+        Me.superButton.Location = New System.Drawing.Point(264, 82)
+        Me.superButton.Name = "superButton"
+        Me.superButton.Size = New System.Drawing.Size(82, 72)
+        Me.superButton.TabIndex = 13
+        Me.superButton.Text = "Super"
+        Me.superButton.UseVisualStyleBackColor = True
+        '
+        'specialButton
+        '
+        Me.specialButton.Location = New System.Drawing.Point(145, 82)
+        Me.specialButton.Name = "specialButton"
+        Me.specialButton.Size = New System.Drawing.Size(82, 72)
+        Me.specialButton.TabIndex = 12
+        Me.specialButton.Text = "Special"
+        Me.specialButton.UseVisualStyleBackColor = True
+        '
+        'regularButton
+        '
+        Me.regularButton.Location = New System.Drawing.Point(31, 82)
+        Me.regularButton.Name = "regularButton"
+        Me.regularButton.Size = New System.Drawing.Size(82, 72)
+        Me.regularButton.TabIndex = 11
+        Me.regularButton.Text = "Regular"
+        Me.regularButton.UseVisualStyleBackColor = True
+        '
+        'totalResultLabel
+        '
+        Me.totalResultLabel.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D
+        Me.totalResultLabel.Location = New System.Drawing.Point(154, 194)
+        Me.totalResultLabel.Name = "totalResultLabel"
+        Me.totalResultLabel.Size = New System.Drawing.Size(121, 33)
+        Me.totalResultLabel.TabIndex = 10
+        Me.totalResultLabel.Text = "$0.00"
+        Me.totalResultLabel.TextAlign = System.Drawing.ContentAlignment.MiddleLeft
+        '
+        'totalLabel
+        '
+        Me.totalLabel.AutoSize = True
+        Me.totalLabel.Location = New System.Drawing.Point(48, 200)
+        Me.totalLabel.Name = "totalLabel"
+        Me.totalLabel.Size = New System.Drawing.Size(82, 20)
+        Me.totalLabel.TabIndex = 9
+        Me.totalLabel.Text = "Total Price:"
+        '
+        'gallonsTextBox
+        '
+        Me.gallonsTextBox.Location = New System.Drawing.Point(169, 16)
+        Me.gallonsTextBox.Name = "gallonsTextBox"
+        Me.gallonsTextBox.Size = New System.Drawing.Size(168, 27)
+        Me.gallonsTextBox.TabIndex = 8
+        '
+        'gallonsLabel
+        '
+        Me.gallonsLabel.AutoSize = True
+        Me.gallonsLabel.Location = New System.Drawing.Point(12, 19)
+        Me.gallonsLabel.Name = "gallonsLabel"
+        Me.gallonsLabel.Size = New System.Drawing.Size(136, 20)
+        Me.gallonsLabel.TabIndex = 7
+        Me.gallonsLabel.Text = "Number of gallons:"
+        '
+        'GasPump
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(374, 259)
+        Me.Controls.Add(Me.superButton)
+        Me.Controls.Add(Me.specialButton)
+        Me.Controls.Add(Me.regularButton)
+        Me.Controls.Add(Me.totalResultLabel)
+        Me.Controls.Add(Me.totalLabel)
+        Me.Controls.Add(Me.gallonsTextBox)
+        Me.Controls.Add(Me.gallonsLabel)
+        Me.Font = New System.Drawing.Font("Segoe UI", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Name = "GasPump"
+        Me.Text = "Gas Pump"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+   Friend WithEvents superButton As System.Windows.Forms.Button
+   Friend WithEvents specialButton As System.Windows.Forms.Button
+   Friend WithEvents regularButton As System.Windows.Forms.Button
+   Friend WithEvents totalResultLabel As System.Windows.Forms.Label
+   Friend WithEvents totalLabel As System.Windows.Forms.Label
+   Friend WithEvents gallonsTextBox As System.Windows.Forms.TextBox
+   Friend WithEvents gallonsLabel As System.Windows.Forms.Label
+
+End Class
---
+++ GasPump/GasPump/GasPump.vb
@@ -0,0 +1,58 @@
+﻿'Gas Pump application
+'Hong Cui
+Option Strict On 'require to convert data type explicitly
+Public Class GasPump
+
+    'global variables are declred outside any nethods
+    'they are used by two or more methods
+    Dim gallons As Integer = 0 ' number of gallons
+    Dim total As Decimal = 0D 'initializa decimal value using letter D 
+    Const REGULAR As Decimal = 2.69D
+    Const SPECIAL As Decimal = 2.79D
+    Const SUPER As Decimal = 2.89D
+
+    ' handles Regular Button's Click event
+    Private Sub regularButton_Click(ByVal sender As System.Object,
+       ByVal e As System.EventArgs) Handles regularButton.Click
+        'get input gallons, convert type exlicitly
+        gallons = Convert.ToInt32(gallonsTextBox.Text)
+        ' call function to calculate total price
+        ' second argument is Button's Text
+        total = Calculate(gallons, regularButton.Text)
+        totalResultLabel.Text = String.Format("{0:C}", total)
+    End Sub ' regularButton_Click
+
+    ' handles Special Button's Click event
+    Private Sub specialButton_Click(ByVal sender As System.Object,
+       ByVal e As System.EventArgs) Handles specialButton.Click
+        gallons = Convert.ToInt32(gallonsTextBox.Text)
+        ' call function to calculate total price
+        total = Calculate(gallons, specialButton.Text)
+        totalResultLabel.Text = String.Format("{0:C}", total)
+    End Sub ' specialButton_Click
+
+    ' handles Super Button's Click event
+    Private Sub superButton_Click(ByVal sender As System.Object,
+       ByVal e As System.EventArgs) Handles superButton.Click
+        gallons = Convert.ToInt32(gallonsTextBox.Text)
+        ' call procedure to calculate total price
+        total = Calculate(gallons, superButton.Text)
+        totalResultLabel.Text = String.Format("{0:C}", total)
+    End Sub ' superButton_Click
+
+    ' calculate total cost of gas
+    Function Calculate(gallons As Integer, grade As String) As Decimal
+        ' determine grade selected and output total
+        Dim totalPrice As Decimal = 0D
+        Select Case grade
+            Case "Regular"
+                totalPrice = Convert.ToDecimal(REGULAR * gallons)
+            Case "Special"
+                totalPrice = Convert.ToDecimal(SPECIAL * gallons)
+            Case "Super"
+                totalPrice = Convert.ToDecimal(SUPER * gallons)
+        End Select
+        Return totalPrice
+    End Function
+End Class
+
